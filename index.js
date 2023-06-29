cbfb1804b08b393bf44f6b5b49ed8d29c1fd1bea
MarynaRak <0633599104s@gmail.com>
2023-06-30 00:33:05 +0300

    preFinal

--- index.js
+++ index.js
@@ -1,65 +1,129 @@
-const inputNode = document.querySelector('.input');
-const addBtnNode = document.querySelector('.add-movie-btn');
+const formNode = document.getElementById('form');
+const inputNode = document.getElementById('user-input');
 const movieListNode = document.getElementById('movie-list-wrapper');
 
-addBtnNode.addEventListener('click', getMovieItem);
+let movies = [];
 
-function getMovieItem() {
-    renderMovieList();
-    clearInput();
-    saveData()
+if (localStorage.getItem('movies')) {
+  movies = JSON.parse(localStorage.getItem('movies'));
 }
 
-movieListNode.addEventListener("click", function(e) {
-  if (e.target.dataset.action === "watched"){
-    e.target.classList.toggle("watched-movie");
-    saveData()
-  } else  if (e.target.tagName === "BUTTON") {
-    const parentNode = e.target.closest('li');
-    parentNode.remove();
-    saveData()
-  } 
-})
+movies.forEach( function (movie) {
+  const cssClass = movie.done ? "movie-item watched-movie" : "movie-item";
 
-function clearInput() {
-    inputNode.value = '';
-}
-
-function renderMovieList() {
-
-    const movieItemTitle = getMovieTitleFromUser();
-    if (!movieItemTitle) {
-        return;
-    }
-
-    const movieItem = `<li data-action="watched" class="movie-item">
-    <label>
-      <input type="checkbox" class="real-checkbox">
-      <span class="circle-checkbox"></span>
-      ${movieItemTitle}
-      <button class="delete-movie-btn"></button>
+  const movieItemHTML = `<li id="${movie.id}" class="${cssClass}">
+    <label data-action="watched" class="flex-input">
+        <input type="checkbox" class="real-checkbox">
+        <span data-action="viewed" class="circle-checkbox"></span>
+        ${movie.text}
+        <button data-action="delete" class="delete-movie-btn"><img class="delete-movie-img" src="images/delete-movie-img.svg" alt="Крестик"/></button>
     </label> 
   </li>`;
 
-    movieListNode.insertAdjacentHTML('beforeend', movieItem);
-}
+  movieListNode.insertAdjacentHTML('beforeend', movieItemHTML);
+});
+
+formNode.addEventListener('submit', addMovieItem);
+
+movieListNode.addEventListener("click", deleteMovieItem);
+
+movieListNode.addEventListener("click", watchedMovieItem);
+
+function addMovieItem(event) {
+
+  event.preventDefault();
+
+  const movieTitle = getMovieTitleFromUser();
+  if (!movieTitle) {
+    return;
+  }
+
+  const newMovieItem = {
+    id: Date.now(),
+    text: movieTitle,
+    done: false,
+  };
+
+  movies.push(newMovieItem);
+
+  saveToLocal();
+
+  const cssClass = newMovieItem.done ? "movie-item watched-movie" : "movie-item";
+
+  const movieItemHTML = `<li id="${newMovieItem.id}" class="${cssClass}">
+    <label data-action="watched" class="flex-input">
+        <input type="checkbox" class="real-checkbox">
+        <span data-action="viewed" class="circle-checkbox"></span>
+        ${newMovieItem.text}
+        <button data-action="delete" class="delete-movie-btn"><img class="delete-movie-img" src="images/delete-movie-img.svg" alt="Крестик"/></button>
+    </label> 
+  </li>`;
+
+  movieListNode.insertAdjacentHTML('beforeend', movieItemHTML);
+
+  clearInput();
+
+
+};
 
 function getMovieTitleFromUser() {
-    const movieTitle = inputNode.value.trim();
+  const movieTitleFromUser = inputNode.value.trim();
 
-    if (movieTitle == 0) {
-        alert('Введите название фильма');
-        return;
-    }
+  if (movieTitleFromUser == 0) {
+    alert('Введите название фильма');
+    return;
+  }
 
-    return movieTitle;
-}
+  return movieTitleFromUser;
+};
 
-function saveData() {
-    localStorage.setItem("data", movieListNode.innerHTML);
-}
+function watchedMovieItem(event) {
 
-function showMovieList() {
-    movieListNode.innerHTML = localStorage.getItem('data');
-}
-showMovieList()
+
+  const parentNode = event.target.closest('li');
+
+  const id = Number(parentNode.id);
+
+  const movie = movies.find(function (movie) {
+    if (movie.id === id)
+      return true;
+  })
+
+  movie.done = !movie.done;
+  saveToLocal();
+
+  if ((event.target.dataset.action === "watched") || (event.target.dataset.action === "viewed")) {
+    event.target.closest('li').classList.toggle("watched-movie");
+  }
+
+
+};
+
+function deleteMovieItem(event) {
+  if (event.target.dataset.action === "delete") {
+    const parentNode = event.target.closest('li');
+
+    const id = Number(parentNode.id);
+
+    movies = movies.filter(function (movie) {
+      if (movie.id !== id) {
+        return true;
+      }
+    })
+
+    saveToLocal();
+
+    parentNode.remove();
+  }
+};
+
+function clearInput() {
+  inputNode.value = '';
+  inputNode.focus()
+};
+
+function saveToLocal() {
+  localStorage.setItem('movies', JSON.stringify(movies))
+};
+
+console.log(movies)
